fdf05bfd69029baf49759324c05e94ec2da34ec5
Dhruv Vishwakarma <dhruvavishw@gmail.com>
2026-07-27 19:42:54 +0530

    Something

---
+++ src/StackPreEvaluation.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class StackPreEvaluation {
+    public static void main(String []BKP){
+        String s="-9/*+5346";
+        Stack<Integer> val=new Stack<>();
+        for(int i=s.length()-1;i>=0;i--){
+            char ch=s.charAt(i);
+            int ascii=(int )ch;
+            if(ascii>=48 && ascii<=57 ){
+                val.push(ascii-48);
+            }
+            else{
+                int v1=val.pop();
+                int v2=val.pop();
+                if(ch=='+') val.push(v1+v2);
+                if(ch=='-') val.push(v1-v2);
+                if(ch=='*') val.push(v1*v2);
+                if(ch=='/') val.push(v1/v2);
+            }
+        }
+        System.out.println(val.peek());
+    }
+}
